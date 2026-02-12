b714a5d68663fb1e5526625928fb5ea96a2c4657
rrfietto <rrfietto@gmail.com>
2026-02-12 19:07:46 -0300

    fixed size calc

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 #define SCREEN_HEIGHT 500
 #define SCREEN_WIDTH 500
 
-#define HEIGHT SCREEN_HEIGHT / 2
-#define WIDTH SCREEN_WIDTH / 2
+#define HEIGHT SCREEN_HEIGHT / 10
+#define WIDTH SCREEN_WIDTH / 10
 
 int screenBuffer[HEIGHT][WIDTH] = {{0}};
 
 void drawPixel(int x, int y)
 {
-    DrawRectangle(x, y, 10, 10, WHITE);
+    DrawRectangle(x * 10, y * 10, 10, 10, WHITE);
     screenBuffer[x][y] = 1;
 }
 
@@ -23,8 +23,8 @@ void grow()
         {
             if (screenBuffer[i][j] == 1 && screenBuffer[i][j - 1] == 0 && screenBuffer[i - 1][j] == 0 && screenBuffer[i + 1][j] == 0 && screenBuffer[i][j + 1] == 0)
             {
-                drawPixel(i - 10, j + 10);
-                drawPixel(i + 10, j + 10);
+                drawPixel(i - 1, j + 1);
+                drawPixel(i + 1, j + 1);
             }
         }
     }
@@ -32,33 +32,40 @@ void grow()
 
 int main()
 {
-    Camera2D cam = {0};
-    cam.zoom = 4.0f;
-
-    int initCellX = 200;
-    int initCellY = 100;
+    int initCellX = 25;
+    int initCellY = 25;
 
     screenBuffer[initCellX][initCellY] = 1;
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cellular Automata");
 
     SetTargetFPS(10);
 
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            std::cout << screenBuffer[i][j];
+        }
+        std::cout << std::endl;
+    }
+
     while (!(WindowShouldClose()))
     {
-
+        ClearBackground(BLACK);
         BeginDrawing();
-        for (int i = 1; i < HEIGHT - 1; i++)
-        {
-            for (int j = 1; j < WIDTH - 1; j++)
-            {
-                if (screenBuffer[i][j] != 0)
-                {
-                    drawPixel(i, j);
-                }
-            }
-        }
+        drawPixel(initCellX, initCellY);
+        // for (int i = 1; i < HEIGHT - 1; i++)
+        // {
+        //     for (int j = 1; j < WIDTH - 1; j++)
+        //     {
+        //         if (screenBuffer[i][j] != 0)
+        //         {
+        //             drawPixel(i, j);
+        //         }
+        //     }
+        // }
         grow();
-        // ClearBackground(BLACK);
+
         EndDrawing();
     }
 
