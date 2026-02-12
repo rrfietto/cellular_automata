b66595ff1aff1108309d46c8473ec27581a0b981
rrfietto <rrfietto@gmail.com>
2026-02-12 20:05:23 -0300

    fully working

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <raylib.h>
 #include <iostream>
 
-#define SCREEN_HEIGHT 500
-#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 800
+#define SCREEN_WIDTH 800
 
 #define HEIGHT SCREEN_HEIGHT / 10
 #define WIDTH SCREEN_WIDTH / 10
 
 int screenBuffer[HEIGHT][WIDTH] = {{0}};
+int prevScreenBuffer[HEIGHT][WIDTH] = {{0}};
 
 void drawPixel(int x, int y)
 {
@@ -15,16 +16,53 @@ void drawPixel(int x, int y)
     screenBuffer[x][y] = 1;
 }
 
+// add growth rules here
+
 void grow()
 {
+    // for (int i = 1; i < HEIGHT - 1; i++)
+    // {
+    //     for (int j = 1; j < WIDTH - 1; j++)
+    //     {
+    //         if (prevScreenBuffer[i][j] == 1 && prevScreenBuffer[i][j - 1] == 0 && prevScreenBuffer[i - 1][j] == 0 && prevScreenBuffer[i + 1][j] == 0 && prevScreenBuffer[i][j + 1] == 0)
+    //         {
+    //             drawPixel(i - 1, j + 1);
+    //             drawPixel(i + 1, j + 1);
+    //         }
+    //     }
+    // }
+
     for (int i = 1; i < HEIGHT - 1; i++)
     {
         for (int j = 1; j < WIDTH - 1; j++)
         {
-            if (screenBuffer[i][j] == 1 && screenBuffer[i][j - 1] == 0 && screenBuffer[i - 1][j] == 0 && screenBuffer[i + 1][j] == 0 && screenBuffer[i][j + 1] == 0)
+            // top and bottom
+            if ((prevScreenBuffer[i][j] == 0) &&
+                (prevScreenBuffer[i - 1][j - 1] == 0) &&
+                (prevScreenBuffer[i - 1][j] == 0) &&
+                (prevScreenBuffer[i][j - 1] == 0) &&
+                (prevScreenBuffer[i][j + 1] == 0) &&
+                (prevScreenBuffer[i + 1][j] == 0) &&
+                (prevScreenBuffer[i + 1][j + 1] == 0) &&
+                ((prevScreenBuffer[i - 1][j + 1] == 1) ||
+                 (prevScreenBuffer[i + 1][j - 1] == 1)) &&
+                (j < 70 && j > 10) && (i < 70 && i > 10))
             {
-                drawPixel(i - 1, j + 1);
-                drawPixel(i + 1, j + 1);
+                drawPixel(i, j);
+            }
+
+            if ((prevScreenBuffer[i][j] == 0) &&
+                (prevScreenBuffer[i - 1][j] == 0) &&
+                (prevScreenBuffer[i - 1][j + 1] == 0) &&
+                (prevScreenBuffer[i][j - 1] == 0) &&
+                (prevScreenBuffer[i][j + 1] == 0) &&
+                (prevScreenBuffer[i + 1][j - 1] == 0) &&
+                (prevScreenBuffer[i + 1][j] == 0) &&
+                ((prevScreenBuffer[i - 1][j - 1] == 1) ||
+                 (prevScreenBuffer[i + 1][j + 1] == 1)) &&
+                (j < 70 && j > 10) && (i < 70 && i > 10))
+            {
+                drawPixel(i, j);
             }
         }
     }
@@ -32,41 +70,39 @@ void grow()
 
 int main()
 {
-    int initCellX = 25;
-    int initCellY = 25;
+    int initCellX = WIDTH / 2;
+    int initCellY = HEIGHT / 2;
 
     screenBuffer[initCellX][initCellY] = 1;
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cellular Automata");
 
-    SetTargetFPS(10);
-
-    for (int i = 0; i < HEIGHT; i++)
-    {
-        for (int j = 0; j < WIDTH; j++)
-        {
-            std::cout << screenBuffer[i][j];
-        }
-        std::cout << std::endl;
-    }
+    SetTargetFPS(5);
 
     while (!(WindowShouldClose()))
     {
-        ClearBackground(BLACK);
         BeginDrawing();
-        drawPixel(initCellX, initCellY);
-        // for (int i = 1; i < HEIGHT - 1; i++)
-        // {
-        //     for (int j = 1; j < WIDTH - 1; j++)
-        //     {
-        //         if (screenBuffer[i][j] != 0)
-        //         {
-        //             drawPixel(i, j);
-        //         }
-        //     }
-        // }
+
+        for (int i = 0; i < HEIGHT; i++)
+        {
+            for (int j = 0; j < WIDTH; j++)
+            {
+                if (screenBuffer[i][j] == 1)
+                {
+                    drawPixel(i, j);
+                }
+            }
+        }
+
         grow();
 
         EndDrawing();
+        for (int i = 0; i < HEIGHT; i++)
+        {
+            for (int j = 0; j < WIDTH; j++)
+            {
+                prevScreenBuffer[i][j] = screenBuffer[i][j];
+            }
+        }
     }
 
     return 0;
